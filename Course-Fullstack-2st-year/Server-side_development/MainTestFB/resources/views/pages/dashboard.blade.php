aaca6ec5a0e978e47290fe4974715fbc0d916e9d
NikitaR041 <nikitar041@gmail.com>
2025-05-20 12:05:14 +0500

    udpate files

--- Course-Fullstack-2st-year/Server-side_development/MainTestFB/resources/views/pages/dashboard.blade.php
+++ Course-Fullstack-2st-year/Server-side_development/MainTestFB/resources/views/pages/dashboard.blade.php
@@ -5,5 +5,4 @@
     <h2>Добро пожаловать, {{ auth()->user()->name }}!</h2>
     <p>Здесь будет ваш рабочий стол.</p>
     <a href="{{ route('logout.form') }}" class="btn btn-secondary">Выход</a>
-
 @endsection
